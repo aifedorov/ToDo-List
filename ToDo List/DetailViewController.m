ed54582904f128eb331857762f0f405a2236f503
alexander <fairk19@gmail.com>
2015-11-12 19:28:12 +0300

    выполнен урок 5

--- ToDo List/DetailViewController.m
+++ ToDo List/DetailViewController.m
@@ -8,8 +8,11 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController ()<UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSave;
 
 @end
 
@@ -17,12 +20,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.buttonSave addTarget:self action:@selector(save) forControlEvents:UIControlEventTouchUpInside];
+    
+    UITapGestureRecognizer * handleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleEndEditing)];
+    [self.view addGestureRecognizer:handleTap];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) handleEndEditing {
+    
+    [self.view endEditing:YES];
+}
+
+- (void) save {
+    NSLog(@"save");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    if ([textField isEqual:self.textField]) {
+            [self.textField resignFirstResponder];
+    }
+    
+    return YES;
 }
 
 @end
